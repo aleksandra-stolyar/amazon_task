11c053b78ef6ccd102529763c9a912326f0aa9b0
Aleksandra <aleksandra.stolyar@gmail.com>
2015-09-14 19:39:53 +0300

    facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,11 +230,11 @@ Devise.setup do |config|
   config.sign_out_via = :get
 
   # ==> OmniAuth
-  if Rails.env == "development" || Rails.env == "test"
-    config.omniauth :facebook, "828115950641942", "f79305c9871fc28573c9a53c8bb335d4", scope: 'email', info_fields: 'email, name'
-  else
+  # if Rails.env == "development" || Rails.env == "test"
+  #   config.omniauth :facebook, "828115950641942", "f79305c9871fc28573c9a53c8bb335d4", scope: 'email', info_fields: 'email, name'
+  # else
     config.omniauth :facebook, "815648321888705", "346c88acb3bc948e3cb62f5edf439ccc", scope: 'email', info_fields: 'email, name'
-  end
+  # end
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
